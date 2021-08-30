7585e4f519c64c94db6de8fd35ae7affdadb8add
Timothy Vieira <vieira.timothy@yahoo.ca>
2021-08-29 20:37:53 -0400

    First commit for project

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  // row and nopadding are props
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a fullstack developer who would be a tremendous asset to your team.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
